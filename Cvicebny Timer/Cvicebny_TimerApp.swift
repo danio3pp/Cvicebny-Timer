87611372478079be0ed870719ff0288ebf8ece2e
Daniel Kovalčík <d.kovalcik@gmail.com>
2024-12-09 09:36:34 +0100

    Initial Commit

---
+++ Cvicebny Timer/Cvicebny_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cvicebny_TimerApp.swift
+//  Cvicebny Timer
+//
+//  Created by Daniel Kovalcik on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cvicebny_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cvicebny TimerTests/Cvicebny_TimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Cvicebny_TimerTests.swift
+//  Cvicebny TimerTests
+//
+//  Created by Daniel Kovalcik on 09.12.2024.
+//
+
+import Testing
+
+struct Cvicebny_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
